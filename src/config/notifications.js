6660fe0b03e98c812b2d8327d2cdead681c5387b
Facu Escobar <facu.escobar@gmail.com>
2018-04-24 12:01:35 -0300

    Fix 4FCCh

--- src/config/notifications.js
+++ src/config/notifications.js
@@ -8,7 +8,7 @@ export default {
       date: new Date(2018, 3, 30, 10),
       repeat: false,
       notification: {
-        title: '4FFCh - Festival de Cine de Chascomús',
+        title: '4FCCh - Festival de Cine de Chascomús',
         body: 'Este jueves 3 arranca el 4FCCh, acercate!',
         ios: {
           sound: true,
@@ -25,7 +25,7 @@ export default {
       date: new Date(2018, 4, 3, 10),
       repeat: false,
       notification: {
-        title: '4FFCh - Festival de Cine de Chascomús',
+        title: '4FCCh - Festival de Cine de Chascomús',
         body: 'Arranca el 4FCCh, te esperamos!',
         ios: {
           sound: true,
